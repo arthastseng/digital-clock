c0230bfed0955bf99c4c1c12efc87439757f83fe
arthastseng <arthastseng@kkcompany.com>
2025-03-13 10:46:20 +0800

    FloatingWIndowUpdate util using SharedFlow

--- app/src/main/kotlin/com/ssc/android/vs_digital_clock/service/FloatingWindowUpdateUtil.kt
+++ app/src/main/kotlin/com/ssc/android/vs_digital_clock/service/FloatingWindowUpdateUtil.kt
@@ -3,15 +3,14 @@ package com.ssc.android.vs_digital_clock.service
 import com.ssc.android.vs_digital_clock.data.TimeZoneInfo
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharedFlow
-import kotlinx.coroutines.flow.StateFlow
 
 import kotlinx.coroutines.launch
 object FloatingWindowUpdateUtil {
 
     private val coroutineScope = CoroutineScope(Dispatchers.IO)
-    private val _dataFlow = MutableStateFlow<List<TimeZoneInfo>>(emptyList())  // 用於更新的 StateFlow
+    private val _dataFlow = MutableSharedFlow<List<TimeZoneInfo>>()  // 用於更新的 StateFlow
     val dataFlow: SharedFlow<List<TimeZoneInfo>> get() = _dataFlow
     // 發送資料
     fun updateData(data: List<TimeZoneInfo>) {
